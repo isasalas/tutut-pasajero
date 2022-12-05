c54b950533bee10f3128c4f2ad814bc1baa4f197
isaias <isasalas145@gmail.com>
2022-12-05 15:57:06 -0400

    interna

--- utils/ApiData.jsx
+++ utils/ApiData.jsx
@@ -1,7 +1,7 @@
 
 //export  const urlApi='http://localhost:3000/api';
-export  const urlSocket='http://44.206.250.164:3001';
-export  const urlApi='http://44.206.250.164:3000/api';
+export  const urlSocket='http://44.210.111.253:3001';
+export  const urlApi='http://44.210.111.253:3000/api';
 export  const urlUser='/user';
 export  const urlLinea='/linea';
 export  const urlRole='/role';
